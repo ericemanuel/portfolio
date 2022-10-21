7f1409111b3182b95fdc469069b54e574e69e857
Eric Emanuel <ericemanuel@gmail.com>
2022-10-21 03:30:03 +0000

    Started writing texts for the projects

--- src/components/quote.vue
+++ src/components/quote.vue
@@ -1,21 +1,35 @@
 <template>
   <article>
     <div class="container">
-      <h1><em>{{ quote }}</em></h1>
+      <h1>
+        <em>{{ quote }}</em>
+      </h1>
     </div>
   </article>
 </template>
 
 <script>
-  export default {
-    name: 'quote',
-    props: ['quote']
-  }
+export default {
+  name: 'quote',
+  props: ['quote']
+}
 </script>
 
 <style scoped lang="scss">
-  h1 {
-      text-align: center;
-      font-size: 24px;
-  }
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  margin-bottom: 5px;
+  text-align: center;
+  font-size: 24px;
+}
+
+span {
+  width: 100%;
+  text-align: right;
+  font-size: 20px;
+}
 </style>
--- src/views/soul.vue
+++ src/views/soul.vue
@@ -1,42 +1,69 @@
 <template>
   <cover id="cover" title="Soul" description="Interfaces com o pensamento"></cover>
 
-  <introduction id="intro" stage="Desafio" title="Interfaces controladas pelo pensamento"
+  <introduction
+    id="intro"
+    stage="Desafio"
+    title="Interfaces controladas pelo pensamento"
     :paragraphs="[
-    'Multiple customer touchpoints provides a wide opportunity for improving the ordering experience.',
-    'After a series of innovation workshops, the team has decided on perfecting the pickup and delivery process by integrating a mobile app for their consumers.',
-    'With the David & Sebs at the helm of it all, the whole team quickly got into the drawing board – drafting, sharing and skeching their ideas.']" location="Rio de Janeiro" date="2014" logo="puc-rio"
-    alt="Puc-Rio" :skills="['Desenvolvimento', 'UX Design', 'Eletrônica']" />
-
-  <stage class="stage" stage="Desafio" title="An MVP for the tech-enabled store" :paragraphs="[
-    'Multiple customer touchpoints provides a wide opportunity for improving the ordering experience.',
-    'After a series of innovation workshops, the team has decided on perfecting the pickup and delivery process by integrating a mobile app for their consumers.',
-    'With the David & Sebs at the helm of it all, the whole team quickly got into the drawing board – drafting, sharing and skeching their ideas.'
-  ]" />
+    'Nós, humanos, percebemos e interagimos com o mundo através dos nossos cinco sentidos. Juntos, eles coletam informações sobre o ambiente que são interpretadas pelo nosso cérebro e nos tornam quem somos.',
+    'Nossos computadores, por outro lado, ainda se limitam a perceber apenas inputs táteis de nossas mãos. Para cada app aberto ou mensagem lida, é necessário uma decisão do usuário e um movimento para ativá-lo.',
+    'Mas, e se não precisasse ser assim?']"
+    location="Rio de Janeiro"
+    date="2014"
+    logo="puc-rio"
+    alt="Puc-Rio"
+    :skills="['Desenvolvimento', 'UX Design', 'Eletrônica']"
+  />
 
   <board id="board" :images="['1', '2', '3']" :alts="['1', '2', '3']" />
 
-  <quote class="quote"
-    quote="In just a few months – they already have branches expanding from Singapore to Bangkok. And now, secretly brewing six more yellow shops near you." />
+  <stage
+    class="stage"
+    stage="Desafio"
+    title="An MVP for the tech-enabled store"
+    :paragraphs="[
+      'Não me entendam mal: mouses, teclados, touch, câmeras e microfones funcionam muito bem no nosso dia-a-dia, mas todos exigem uma ação do usuário para serem compreendidas. Até mesmo a Alexa exige que você fale o nome dela.',
+      'Nossa comunicação com outras pessoas não funciona assim. Muitas informações são transmitidas de forma espontânea e inconsciente - como uma postura corporal, um tom de voz ou um simples olhar.',
+      'Mas é claro que esses dados existem em algum lugar: o nosso cérebro. '
+    ]"
+  />
 
-  <stage class="stage" stage="Desafio" title="An MVP for the tech-enabled store" :paragraphs="[
-    'Multiple customer touchpoints provides a wide opportunity for improving the ordering experience.',
-    'After a series of innovation workshops, the team has decided on perfecting the pickup and delivery process by integrating a mobile app for their consumers.',
-    'With the David & Sebs at the helm of it all, the whole team quickly got into the drawing board – drafting, sharing and skeching their ideas.'
-  ]" />
+  <quote
+    class="quote"
+    quote="&quot;Se eu pudesse resumir o seu projeto em uma palavra, escolheria Resiliência&quot;"
+  />
 
-  <gallery class="gallery" :images="['1', '1', '1', '1', '1', '1']" :alts="['1', '2', '3', '1', '2', '3']" />
+  <gallery
+    class="gallery"
+    :images="['1', '1', '1', '1', '1', '1']"
+    :alts="['1', '2', '3', '1', '2', '3']"
+  />
 
-  <stage class="stage" stage="Desafio" title="An MVP for the tech-enabled store" :paragraphs="[
-    'Multiple customer touchpoints provides a wide opportunity for improving the ordering experience.',
-    'After a series of innovation workshops, the team has decided on perfecting the pickup and delivery process by integrating a mobile app for their consumers.',
-    'With the David & Sebs at the helm of it all, the whole team quickly got into the drawing board – drafting, sharing and skeching their ideas.'
-  ]" />
+  <stage
+    class="stage"
+    stage="Desafio"
+    title="An MVP for the tech-enabled store"
+    :paragraphs="[
+      'Multiple customer touchpoints provides a wide opportunity for improving the ordering experience.',
+      'After a series of innovation workshops, the team has decided on perfecting the pickup and delivery process by integrating a mobile app for their consumers.',
+      'With the David & Sebs at the helm of it all, the whole team quickly got into the drawing board – drafting, sharing and skeching their ideas.'
+    ]"
+  />
 
-  <gallery class="gallery" :images="['1', '1', '1', '1', '1', '1']" :alts="['1', '2', '3', '1', '2', '3']" />
+  <gallery
+    class="gallery"
+    :images="['1', '1', '1', '1', '1', '1']"
+    :alts="['1', '2', '3', '1', '2', '3']"
+  />
 
-  <others id="others" :links="['./montehermom', './designsystem']" :titles="['Monte Hermom', 'Design System']"
-    :images="['1', '2']" :alts="['1', '2']" />
+  <others
+    id="others"
+    :links="['./montehermom', './designsystem']"
+    :titles="['Monte Hermom', 'Design System']"
+    :images="['1', '2']"
+    :alts="['1', '2']"
+  />
 </template>
 
 <script>
